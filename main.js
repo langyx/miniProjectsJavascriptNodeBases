f6dd66663e856860b1a55cbe00c7e0b1aac6b107
Yannis Lang <jopolaz@hotmail.fr>
2019-06-05 10:03:02 +0200

    Main Explications

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-let readlineSync = require('readline-sync');
+/**
+ * Le price right utilise la récursive pour lire l'entrée utilisateur
+ * Vous pouvez également utiliser un package qui permet de lire l'entrée utilisateur
+ * de manière synchrone dans une boucle
+ */
 
 
-while (true)  {
-    let input = readlineSync.question(">");
-    console.log(input);
-}
+/**
+ * Le concept syncrhone / asynchrone => https://www.mathieu-martinot.com/wp-content/uploads/2016/12/synchrone-vs-asynchrone.png
+ */
 
+
+/**
+ * Les packages npm => https://formationjavascript.com/guides/npm-node-package-manager/
+ */
+
+
+/**
+ * Lancer vos projets en ligne de commande Node (!du navigateur) =>
+ * Windows => https://openclassrooms.com/fr/courses/1056721-des-applications-ultra-rapides-avec-node-js/1056956-installer-node-js
+ * Linux => https://blog.teamtreehouse.com/install-node-js-npm-linux
+ * MacOS => https://nodesource.com/blog/installing-nodejs-tutorial-mac-os-x/
+ */
+
+
+/**
+ * La fonction SetInterval => https://www.w3schools.com/jsref/met_win_setinterval.asp
+ */
